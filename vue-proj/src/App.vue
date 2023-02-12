d4bc3f1292fe1e19558d87639d2111448857ac8a
b-sai <bsaishreyas@gmail.com>
2023-02-12 13:23:20 -0600

    create component

--- vue-proj/src/App.vue
+++ vue-proj/src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <h1>{{title}}</h1>
+  <Question />
 </template>
 
+
 <script>
-
-
+import Question from './components/Question.vue'
 export default {
   name: 'App',
-  data (){
-    return{
-      title: "Hello World"
-    }
-  }
+  components: {
+    Question
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1{
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
-}
-</style>
